3482118dec4ce99a9ebd7d9757f769f54efb1244
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-05-16 15:45:57 +0200

    [add] stampa comics in pagina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,33 @@
 @extends('layout.main')
 
 @section('content')
+<div class="container">
+    <h1>LIsta CD comics</h1>
+</div>
 
-<h1>Home</h1>
+
+
+
+<div class="container d-flex ">
+    <div class="row   ">
+        @foreach ($comics as $comic)
+            <div class="card m-3" style="width: 18rem;">
+                <img src="{{$comic->thumb}}" class="card-img-top" alt="{{$comic->title}}">
+                <div class="card-body">
+                    <h5 class="card-title">{{$comic->title}}</h5>
+                    <p class="card-text">Data di uscita: {{$comic->sale_date}}</p>
+                    <p class="card-text">Tipologia: {{$comic->type}}</p>
+                    <p class="card-text">Prezzo: {{$comic->price}}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+</div>
+
+
+
 
 @endsection
 
