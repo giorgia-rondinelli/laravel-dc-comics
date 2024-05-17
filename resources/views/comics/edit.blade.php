c6889428c920c22695e65a5bb8538c5c3d9d3909
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-05-17 18:48:27 +0200

    [fixed] edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -6,51 +6,51 @@
     <h3>Modifica comic</h3>
     <form class="" action="{{route('comics.update',$comic)}}" method="POST">
         @csrf
-        @method('PUT')
+        @method('Put')
 
             <div class="mb-3">
               <label for="title" class="form-label pt-3 ">Titolo</label>
-              <input name="title" type="text" class="form-control" id="title" aria-describedby="">
+              <input value="{{$comic->title}}" name="title" type="text" class="form-control" id="title" aria-describedby="">
 
             </div>
             <div class="mb-3">
               <label for="description" class="form-label pt-3 ">Descrizione</label>
-              <input name="description" type="text" class="form-control" id="description" aria-describedby="">
+              <input value="{{$comic->description}}" name="description" type="text" class="form-control" id="description" aria-describedby="">
 
             </div>
             <div class="mb-3">
               <label for="thumb" class="form-label  ">Immagine</label>
-              <input name="thumb" type="text" class="form-control" id="thumb" aria-describedby="">
+              <input name="thumb"value="{{$comic->thumb}} type="text" class="form-control" id="thumb" aria-describedby="">
 
             </div>
             <div class="mb-3">
               <label for="price" class="form-label  ">Prezzo</label>
-              <input name="price" type="text" class="form-control" id="price" aria-describedby="">
+              <input value="{{$comic->price}} name="price" type="text" class="form-control" id="price" aria-describedby="">
 
             </div>
             <div class="mb-3">
               <label for="series" class="form-label  ">Serie</label>
-              <input name="series" type="text" class="form-control" id="series" aria-describedby="">
+              <input value="{{$comic->series}} name="series" type="text" class="form-control" id="series" aria-describedby="">
 
             </div>
             <div class="mb-3">
               <label for="sale_date" class="form-label  ">Data di uscita</label>
-              <input name="sale_date" type="text" class="form-control" id="sale_date" aria-describedby="">
+              <input value="{{$comic->sale_date}} name="sale_date" type="text" class="form-control" id="sale_date" aria-describedby="">
 
             </div>
             <div class="mb-3">
               <label for="type" class="form-label  ">Tipologia</label>
-              <input name="type" type="text" class="form-control" id="type" aria-describedby="">
+              <input value="{{$comic->type}} name="type" type="text" class="form-control" id="type" aria-describedby="">
 
             </div>
             <div class="mb-3">
               <label for="writers" class="form-label  ">Scrittori</label>
-              <input name="writers" type="text" class="form-control" id="writers" aria-describedby="">
+              <input value="{{$comic->writers}} name="writers" type="text" class="form-control" id="writers" aria-describedby="">
 
             </div>
             <div class="mb-3">
               <label for="artists" class="form-label  ">Artisti</label>
-              <input name="artists" type="text" class="form-control" id="artists" aria-describedby="">
+              <input value="{{$comic->artists}} name="artists" type="text" class="form-control" id="artists" aria-describedby="">
 
             </div>
 
